7db57cd81fec1022fd59b9fac3cbcbba237766c8
Piotr Chudziak <strangelove44@gmail.com>
2019-05-07 09:11:03 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Hello extends React.Component {
+  render() {
+  return (
+      <div>
+      <p>Hello there</p>
+      </div>
+
+    );
+   }
+ }
+
+export default Hello;
