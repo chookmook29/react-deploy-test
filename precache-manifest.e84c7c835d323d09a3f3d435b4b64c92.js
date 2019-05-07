e7a0a82735891f9f055412c1f67ec6a153bb0c7f
Piotr Chudziak <strangelove44@gmail.com>
2019-05-07 09:18:23 +0100

    Updates

---
+++ precache-manifest.e84c7c835d323d09a3f3d435b4b64c92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5993c87d0913877e35b1588a892afcf6",
+    "url": "/react-deploy-test/index.html"
+  },
+  {
+    "revision": "65c34cb52b927cc0b73b",
+    "url": "/react-deploy-test/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-deploy-test/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "65c34cb52b927cc0b73b",
+    "url": "/react-deploy-test/static/js/main.42222748.chunk.js"
+  },
+  {
+    "revision": "159089ec984b0660a05a",
+    "url": "/react-deploy-test/static/js/runtime~main.57368b07.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
